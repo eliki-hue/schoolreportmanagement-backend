395fa0db7eab991e574f52a7dea7349f52494bab
ELIJAH KIRAGU <eliki13720@gmail.com>
2023-03-27 18:06:30 +0300

    connected database

--- schoolreportmanagement/settings.py
+++ schoolreportmanagement/settings.py
@@ -74,11 +74,17 @@ WSGI_APPLICATION = 'schoolreportmanagement.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
+       
+   }
+
 
 
 # Password validation
