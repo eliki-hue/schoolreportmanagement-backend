fb659659498c41eb3419a85e8f9afdae5c4a49ac
ELIJAH KIRAGU <eliki13720@gmail.com>
2023-03-27 17:46:45 +0300

    created student score

--- resultManager/models.py
+++ resultManager/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+
+class Score(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='scores')
+    subject = models.CharField(max_length=255)
+    score = models.IntegerField()
