7bd44541ca7643452ed501a7e7542c9b8f53062e
ELIJAH KIRAGU <eliki13720@gmail.com>
2023-03-27 17:59:02 +0300

    created submit student data view

--- resultManager/views.py
+++ resultManager/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import StudentSerializer
+
+@api_view(['POST'])
+def submit_student_data(request):
+    serializer = StudentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    else:
+        return Response(serializer.errors, status=400)
